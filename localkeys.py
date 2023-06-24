29ac348e3a8c2aecc71dc2f608e10a67f577e22c
Saksham Kumar <sakshamkumarprasad03@gmail.com>
2023-06-24 22:36:16 +0530

    Publish

---
+++ localkeys.py
@@ -0,0 +1,15 @@
+#This file will contain all the local values not to be shared with the public
+
+# This is my key for the Open Weather Map API
+
+def get_key():
+    API_KEY = "e1b382aabc1de3e277339f1b19b9a513"
+    return API_KEY
+
+#Syntax:
+'''
+def get_key():
+    API_KEY=<Your API Key>
+    return API_KEY
+'''
+
